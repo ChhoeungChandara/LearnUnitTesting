922b9d215bf4e72737ac44b00931046a2542957d
chandara <115058826+ChhoeungChandara@users.noreply.github.com>
2024-02-26 19:56:16 +0700

    write some test

--- src/main/java/com/chandara/UnitTesting/Collections/ListsDemo.java
+++ src/main/java/com/chandara/UnitTesting/Collections/ListsDemo.java
@@ -1,25 +1,43 @@
 package com.chandara.UnitTesting.Collections;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ListsDemo {
-	
-	public static List<Integer> toIntegerList(List<String> list){
-		
+	//1
+	public static List<Integer> StringtoIntegerList(List<String> list){
 		return list.stream().map(string->string.length())
 				            .toList();
 	}
+	//2
 	
 	public static List<Integer> getEvenNumber(List<Integer> list){
 		return list.stream().filter(num->num%2==0).toList(); 
 	}
-	
+	//3
 	public static List<Integer> getOldNumber(List<Integer> list){
 		return list.stream().filter(num->num%2!=0).toList();
 	}
+	//4
 	
 	public static List<String> sortString(List<String> list){
 		return list.stream().sorted().toList();
+	}
+	//5
+	
+	public static List<Integer> UingSkipAndLimitOperations(List<Integer> list){
+		return list.stream().skip(2).limit(5).toList();
+	}
+	//6
+	public static List<Integer> sumOfTwoList(List<Integer> list1,List<Integer> list2){
+		List<Integer> result = new ArrayList<>();
+		int minSize = Math.min(list1.size(),list2.size()); // [1,2,3] and [1,2,3,4] // 4 no need to sum
+		result = IntStream.range(0, minSize)
+				          .mapToObj(i->list1.get(i)+list2.get(i))
+				          .collect(Collectors.toList());
+		return result;
 	}
 
 }
--- src/test/java/com/chandara/UnitTesting/ListDemoTesting/ListDemoTesting.java
+++ src/test/java/com/chandara/UnitTesting/ListDemoTesting/ListDemoTesting.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 import com.chandara.UnitTesting.Collections.ListsDemo;
 public class ListDemoTesting {
 	
+	//1
 	@Test
 	public void convertListOfStringToListOfInteger() {
 		
 		List<String> string = List.of("apple","banana","hello");
-		List<Integer> integerList = ListsDemo.toIntegerList(string);
+		List<Integer> integerList = ListsDemo.StringtoIntegerList(string);
 		assertEquals(3,integerList.size());
 		assertEquals(5,integerList.get(0));
 		assertEquals(6,integerList.get(1));
 		assertEquals(5,integerList.get(2));
 	}
-	
+	//2
 	@Test
 	public void getEvenNumberTest() {
 		List<Integer> num = List.of(4,6,7,8);
@@ -24,7 +25,7 @@ public class ListDemoTesting {
 		assertEquals(3,evenNumber.size());
 		assertEquals(expectedNum,evenNumber);
 	}
-	
+	//3
 	@Test
 	public void getOldNumberTest() {
 		List<Integer> num = List.of(3,4,5,6,8,9,11);
@@ -33,7 +34,7 @@ public class ListDemoTesting {
 		assertEquals(4, oldNum.size());
 		assertEquals(expectedNum,oldNum);
 	}
-	
+	//4
 	@Test
 	public void sortStringTest() {
 		List<String> strings = List.of("apple","egg","phone","banana");
@@ -42,5 +43,25 @@ public class ListDemoTesting {
 		assertEquals(4, sortList.size());
 		assertEquals(expectList,sortList);
 	}
+	//6
+	@Test
+	public void sumOfTwoList() {
+		List<Integer> list1 = List.of(1,2,3);
+		List<Integer> list2 = List.of(3,4,6);
+		List<Integer> expectedResult = List.of(4,6,9);
+		List<Integer> sumOfTwoList = ListsDemo.sumOfTwoList(list1, list2);
+		assertEquals(3,sumOfTwoList.size());
+		assertEquals(expectedResult,sumOfTwoList);
+	}
+	//5
+	@Test
+	public void skipAndLimitOperatiosTest() {
+		List<Integer> lists = List.of(1,2,3,4,5,6,7,8,11,23);
+		List<Integer> expectedResult = List.of(3,4,5,6,7);
+		List<Integer> list1 = ListsDemo.UingSkipAndLimitOperations(lists);
+		assertEquals(5,list1.size());
+		assertEquals(expectedResult, list1);
+	}
+	
 
 }
